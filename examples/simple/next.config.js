298a6d8223c52d6a2385800928130712b6b958a1
Emanuel Kluge <emanuel.kluge@jimdo.com>
2017-09-05 22:21:06 +0200

    refactor: update next-config of simple example

--- examples/simple/next.config.js
+++ examples/simple/next.config.js
@@ -1,9 +1,7 @@
-const { getPathMap, writeData } = require('phox/export');
+const getPathMap = require('phox/export');
 
 module.exports = {
   async exportPathMap() {
-    const paths = await getPathMap();
-    await writeData(paths);
-    return paths;
+    return await getPathMap();
   },
 };

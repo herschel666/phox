b7425bb70c1e740a5771f475af058f0346459c8d
herschel666 <kraemerseele@gmx.de>
2019-03-12 19:22:55 +0100

    chore: update Jest to v24

--- test/export/export.spec.ts
+++ test/export/export.spec.ts
@@ -1,26 +1,18 @@
 import fetch from 'node-fetch';
 import { html, json, url, ignoreThingies } from '../util';
 
-describe('Export HTML', async () => {
-  await Promise.all(
-    html.map((pathName: string) =>
-      test(`Renders ${pathName}`, async () => {
-        const response = await fetch(url(pathName));
-        const result = await response.text();
-        expect(ignoreThingies(result)).toMatchSnapshot();
-      })
-    )
-  );
+describe('Export HTML', () => {
+  test.each(html)('Renders %s', async (pathName: string) => {
+    const response = await fetch(url(pathName));
+    const result = await response.text();
+    expect(ignoreThingies(result)).toMatchSnapshot();
+  });
 });
 
-describe('Export JSON', async () => {
-  await Promise.all(
-    json.map((pathName: string) =>
-      test(`Renders ${pathName}`, async () => {
-        const response = await fetch(url(pathName));
-        const result = await response.json();
-        expect(ignoreThingies(JSON.stringify(result))).toMatchSnapshot();
-      })
-    )
-  );
+describe('Export JSON', () => {
+  test.each(json)('Renders %s', async (pathName: string) => {
+    const response = await fetch(url(pathName));
+    const result = await response.json();
+    expect(ignoreThingies(JSON.stringify(result))).toMatchSnapshot();
+  });
 });
